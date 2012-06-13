7775fb7041f57b7fb04332219dfe520f270b59c9
jwgameapp <jwgameapp@qq.com>
2012-06-13 10:51:13 +0800

    update Hello class
    Signed-off-by: jwgameapp <jwgameapp@qq.com>

--- Son1/src/main/java/Hello.java
+++ Son1/src/main/java/Hello.java
@@ -8,6 +8,13 @@ public class Hello
 	public static void main(String[] args)
 	{
 		System.out.println("Hello world!!!!");
+		System.out.println("Hello world!!!!");
+		System.out.println("Hello world!!!!");
+		System.out.println("Hello world!!!!");
+		System.out.println("Hello world!!!!");
+		System.out.println("Hello world!!!!");
+		System.out.println("Hello world!!!!");
+		System.out.println("Hello world!!!!");
 
 	}
 
